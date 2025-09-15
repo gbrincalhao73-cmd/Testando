a3f05556fa48ad5ff20fbbcd7d5249d259600873
Gustavo N. <gbrincalhao73-cmd>
2025-09-15 18:56:27 -0300

    primeiro commit galera

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 public class Main {
+
+    public static int rolador() {
+        Random rd = new Random();
+        return rd.nextInt(1,20);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Olá!");
+        System.out.println("Olá! Você tirou " + rolador());
     }
 }
