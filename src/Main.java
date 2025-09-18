bbe8a3d526c5a139d79b35a38425f253909bbdf8
Gustavo N. <gbrincalhao73@gmail.com>
2025-09-18 20:50:03 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ import java.util.Random;
 
 public class Main {
 
-    public static int rolador() {
+    public static int rolador(int lados) {
         Random rd = new Random();
-        return rd.nextInt(1,20);
+        return rd.nextInt(1, lados);
     }
 
     public static void main(String[] args) {
         System.out.println("Olá! Você tirou " + rolador());
-        SYstem.out.println("Taca a mãe pra ver se quica glenn."):
+        SYytem.out.println("Taca a mãe pra ver se quica glenn.");
+         System.out.println(rolador(2));
+         System.out.println(rolador(4);
+         System.out.println(rolador(6));
+         System.out.println(rolador(6));
+         System.out.println(rolador(8));
+        
     }
 }
