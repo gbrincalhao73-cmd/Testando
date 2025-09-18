b05e1a17649f79284563c0c03eceb016208d05f0
Gustavo N. <gbrincalhao73@gmail.com>
2025-09-18 20:23:04 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Olá! Você tirou " + rolador());
+        SYstem.out.println("Taca a mãe pra ver se quica glenn."):
     }
 }
