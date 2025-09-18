4308f6827f19ef765e5468d847a4cafa427582b2
Gustavo N. <gbrincalhao73-cmd>
2025-09-18 20:14:52 -0300

    ultimo teste

--- src/Casa.java
+++ src/Casa.java
@@ -2,4 +2,9 @@ public class Casa {
     String endereço;
     int comodos;
 
+    Casa(String endereco, int comodos) {
+        this.endereço = endereco;
+        this.comodos = comodos;
+    }
+
 }
