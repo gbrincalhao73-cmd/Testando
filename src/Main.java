35bce9f7c3820f270321248d3067995874fd3662
Gustavo N. <gbrincalhao73-cmd>
2025-09-18 21:29:10 -0300

    123

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,15 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Olá! Você tirou " + rolador());
-        Sytem.out.println("Taca a mãe pra ver se quica glenn.");
+        System.out.println("Olá! Você tirou " + rolador(100000));
+        System.out.println("Taca a mãe pra ver se quica glenn.");
         System.out.println(rolador(100));
-         System.out.println(rolador(2));
-         System.out.println(rolador(4);
-         System.out.println(rolador(6));
-         System.out.println(rolador(8));
-         int[] partes = new int[60];
+        System.out.println(rolador(2));
+        System.out.println(rolador(4));
+        System.out.println(rolador(6));
+        System.out.println(rolador(8));
+        int[] partes = new int[60];
+
         
     }
 }
